c0fd6c8d96e329be4d337a165acebffc0b9e4659
ArikReuter <arik_reuter@gmx.de>
2024-03-10 17:56:23 +0100

    Refactor negative log likelihood methods in ModelPosterior

--- LinearRegression/Models/ModelPosterior.py
+++ LinearRegression/Models/ModelPosterior.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 import torch
 
 
+from PFNExperiments.Training.EvalMetrics import mean, median, std
+
+
 class ModelPosterior(ABC):
     """
     Class that takes in a model and returns the posterior the model provides
@@ -29,7 +32,7 @@ class ModelPosterior(ABC):
         """
         pass
 
-    def negative_log_likelihood_avg(self, list_of_preds, list_of_targets: list[torch.Tensor]) -> torch.Tensor:
+    def negative_log_likelihood_list(self, list_of_preds, list_of_targets: list[torch.Tensor]) -> list[float]:
         """
         A method that takes in the models prediction and the target and returns the negative log likelihood averaged over the batch
         Args:
@@ -38,12 +41,51 @@ class ModelPosterior(ABC):
         Returns:
             torch.Tensor: the negative log likelihood
         """
-        nll = 0
-        for i in range(len(list_of_preds)):
-            nll += self.negative_log_likelihood(list_of_preds[i], list_of_targets[i])
-        return nll / len(list_of_preds)
+        nll_list = []
+        for pred, target in zip(list_of_preds, list_of_targets):
+            nll_list.append(self.negative_log_likelihood(pred, target).item())
+        
+        return nll_list
+    
+    def negative_log_likelihood_avg(self, list_of_preds, list_of_targets: list[torch.Tensor]) -> float:
+        """
+        A method that takes in the models prediction and the target and returns the negative log likelihood averaged over the batch
+        Args:
+            list_of_preds: list[torch.Tensor]: the models prediction
+            list_of_targets: list[torch.Tensor]: the target
+        Returns:
+            torch.Tensor: the negative log likelihood
+        """
+        nll_list = self.negative_log_likelihood_list(list_of_preds, list_of_targets)
+        return mean(nll_list)
+    
+    def negative_log_likelihood_std(self, list_of_preds, list_of_targets: list[torch.Tensor]) -> float:
+        """
+        A method that takes in the models prediction and the target and returns the standard deviation of the negative log likelihood over the batch
+        Args:
+            list_of_preds: list[torch.Tensor]: the models prediction
+            list_of_targets: list[torch.Tensor]: the target
+        Returns:
+            torch.Tensor: the negative log likelihood
+        """
+        nll_list = self.negative_log_likelihood_list(list_of_preds, list_of_targets)
+        return std(nll_list)
+    
+    def negative_log_likelihood_median(self, list_of_preds, list_of_targets: list[torch.Tensor]) -> float:
+        """
+        A method that takes in the models prediction and the target and returns the median of the negative log likelihood over the batch
+        Args:
+            list_of_preds: list[torch.Tensor]: the models prediction
+            list_of_targets: list[torch.Tensor]: the target
+        Returns:
+            torch.Tensor: the negative log likelihood
+        """
+        nll_list = self.negative_log_likelihood_list(list_of_preds, list_of_targets)
+        return median(nll_list)
     
     
+    
+
     def pred2posterior_samples(self, pred, n_samples: int) -> torch.Tensor:
         """
         A method that takes in the models prediction and returns samples from the posterior distribution
--- Training/EvalMetrics.py
+++ Training/EvalMetrics.py
@@ -4,50 +4,198 @@ Class to implement a few evaluation metrics
 
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import torch
+import numpy as np
+
+def mean(list: list[float]) -> float:
+    """
+    Compute the mean of a list
+    Args:
+        list: list[float]: the list of numbers
+    Returns:
+        float: the mean
+    """
+    return sum(list) / len(list)
+
+def std(list: list[float]) -> float:
+    """
+    Compute the standard deviation of a list
+    Args:
+        list: list[float]: the list of numbers
+    Returns:
+        float: the standard deviation
+    """
+    m = mean(list)
+    return (sum((x - m) ** 2 for x in list) / len(list)) ** 0.5
+
+def median(list: list[float]) -> float:
+    """
+    Compute the median of a list
+    Args:
+        list: list[float]: the list of numbers
+    Returns:
+        float: the median
+    """
+    median = np.median(list)
+
+    return median
+
+
+def mean_squared_error_torch_list(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> list[float]:
+    """
+    Compute the mean squared error for each output
+    Args:
+        y_true: torch.tensor: the true values
+        y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
+    Returns:
+        list[float]: the mean squared error for each output
+    """
+    mse = []
+    for i in range(len(y_pred[0])):
+        mse.append(mean_squared_error(y_true.detach().numpy(), y_pred[0][i].detach().numpy()))
+    return mse
 
 def mean_squared_error_torch_avg(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> float:
     """
-    Compute the mean squared error averaged over the batch
+    Compute the mean squared error averaged over all outputs
     Args:
         y_true: torch.tensor: the true values
         y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
     Returns:
-        float: the mean squared error
+        float: the mean squared error averaged over all outputs
     """
-    mse = 0
-    for i in range(len(y_pred)):
-        
-        assert y_true[i].detach().numpy().shape == y_pred[i][0].detach().numpy().shape, f"y_true shape {y_true[i].detach().numpy().shape} does not match y_pred shape {y_pred[i][0].detach().numpy().shape}"
-        mse += mean_squared_error(y_true[i].detach().numpy(), y_pred[i][0].detach().numpy())
-    return mse / len(y_pred)
-    
+    mse_list = mean_squared_error_torch_list(y_true, y_pred)
+    mean_mse = mean(mse_list)
+    return mean_mse
+
+def mean_squared_error_torch_std(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> float:
+    """
+    Compute the standard deviation of the mean squared error over all outputs
+    Args:
+        y_true: torch.tensor: the true values
+        y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
+    Returns:
+        float: the standard deviation of the mean squared error over all outputs
+    """
+    mse_list = mean_squared_error_torch_list(y_true, y_pred)
+    std_mse = std(mse_list)
+    return std_mse
+
+def mean_squared_error_torch_median(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> float:
+    """
+    Compute the median of the mean squared error over all outputs
+    Args:
+        y_true: torch.tensor: the true values
+        y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
+    Returns:
+        float: the median of the mean squared error over all outputs
+    """
+    mse_list = mean_squared_error_torch_list(y_true, y_pred)
+    median_mse = median(mse_list)
+    return median_mse
+
+def r2_score_torch_list(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> list[float]:
+    """
+    Compute the r2 score for each output
+    Args:
+        y_true: torch.tensor: the true values
+        y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
+    Returns:
+        list[float]: the r2 score for each output
+    """
+    r2 = []
+    for i in range(len(y_pred[0])):
+        r2.append(r2_score(y_true.detach().numpy(), y_pred[0][i].detach().numpy()))
+    return r2
 
 def r2_score_torch_avg(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> float:
     """
-    Compute the r2 score averaged over the batch
+    Compute the r2 score averaged over all outputs
     Args:
         y_true: torch.tensor: the true values
         y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
     Returns:
-        float: the r2 score
+        float: the r2 score averaged over all outputs
     """
-    r2 = 0
-    for i in range(len(y_pred)):
-        assert y_true[i].detach().numpy().shape == y_pred[i][0].detach().numpy().shape, f"y_true shape {y_true[i].detach().numpy().shape} does not match y_pred shape {y_pred[i][0].detach().numpy().shape}"
-        r2 += r2_score(y_true[i].detach().numpy(), y_pred[i][0].detach().numpy())
-    return r2 / len(y_pred)
+    r2_list = r2_score_torch_list(y_true, y_pred)
+    mean_r2 = mean(r2_list)
+    return mean_r2
+
+def r2_score_torch_std(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> float:
+    """
+    Compute the standard deviation of the r2 score over all outputs
+    Args:
+        y_true: torch.tensor: the true values
+        y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
+    Returns:
+        float: the standard deviation of the r2 score over all outputs
+    """
+    r2_list = r2_score_torch_list(y_true, y_pred)
+    std_r2 = std(r2_list)
+    return std_r2
+
+def r2_score_torch_median(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> float:
+    """
+    Compute the median of the r2 score over all outputs
+    Args:
+        y_true: torch.tensor: the true values
+        y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
+    Returns:
+        float: the median of the r2 score over all outputs
+    """
+    r2_list = r2_score_torch_list(y_true, y_pred)
+    median_r2 = median(r2_list)
+    return median_r2
+
+def mae_torch_list(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> list[float]:
+    """
+    Compute the mean absolute error for each output
+    Args:
+        y_true: torch.tensor: the true values
+        y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
+    Returns:
+        list[float]: the mean absolute error for each output
+    """
+    mae = []
+    for i in range(len(y_pred[0])):
+        mae.append(mean_absolute_error(y_true.detach().numpy(), y_pred[0][i].detach().numpy()))
+    return mae
+
 
 def mae_torch_avg(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> float:
     """
-    Compute the mean absolute error averaged over the batch
+    Compute the mean absolute error averaged over all outputs
     Args:
         y_true: torch.tensor: the true values
         y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
     Returns:
-        float: the mean absolute error
+        float: the mean absolute error averaged over all outputs
     """
-    mae = 0
-    for i in range(len(y_pred)):
-        assert y_true[i].detach().numpy().shape == y_pred[i][0].detach().numpy().shape, f"y_true shape {y_true[i].detach().numpy().shape} does not match y_pred shape {y_pred[i][0].detach().numpy().shape}"
-        mae += mean_absolute_error(y_true[i].detach().numpy(), y_pred[i][0].detach().numpy())
-    return mae / len(y_pred)
+    mae_list = mae_torch_list(y_true, y_pred)
+    mean_mae = mean(mae_list)
+    return mean_mae
+
+def mae_torch_std(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> float:
+    """
+    Compute the standard deviation of the mean absolute error over all outputs
+    Args:
+        y_true: torch.tensor: the true values
+        y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
+    Returns:
+        float: the standard deviation of the mean absolute error over all outputs
+    """
+    mae_list = mae_torch_list(y_true, y_pred)
+    std_mae = std(mae_list)
+    return std_mae
+
+def mae_torch_median(y_true: torch.tensor, y_pred: list[list[torch.tensor]]) -> float:
+    """
+    Compute the median of the mean absolute error over all outputs
+    Args:
+        y_true: torch.tensor: the true values
+        y_pred: list[list[torch.tensor]]: the predicted values where the first list is the batch and the second list is the output that CAN contain multiple outputs
+    Returns:
+        float: the median of the mean absolute error over all outputs
+    """
+    mae_list = mae_torch_list(y_true, y_pred)
+    median_mae = median(mae_list)
+    return median_mae
