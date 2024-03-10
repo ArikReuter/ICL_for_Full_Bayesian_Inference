8043fca5f327d644fb8fb3ac087606c46382aa3b
ArikReuter <arik_reuter@gmx.de>
2024-03-10 10:49:40 +0100

    Add testset and test method to Trainer class

--- Training/Trainer.py
+++ Training/Trainer.py
@@ -23,6 +23,7 @@ class Trainer():
                  loss_function: torch.nn.modules.loss._Loss,
                  trainset: torch.utils.data.Dataset,
                  valset: torch.utils.data.Dataset,
+                 testset: torch.utils.data.Dataset = None,
                  batch_to_model_function: callable = batch_to_model_lm,
                  target_key_in_batch: str = "beta",
                  evaluation_functions: dict[str, callable] = {},
@@ -39,6 +40,7 @@ class Trainer():
             loss_function: torch.nn.modules.loss._Loss: the loss function
             trainset: torch.utils.data.Dataset: the training set
             valset: torch.utils.data.Dataset: the validation set
+            testset: torch.utils.data.Dataset: the test set
             batch_to_model_function: callable: a function that maps a batch to the model input
             target_key_in_batch: str: the key of the target in the batch
             evaluation_functions: dict[str, callable]: the evaluation functions in a dictionary where the key is the name of the evaluation function and the value is the evaluation function
@@ -52,6 +54,7 @@ class Trainer():
         self.loss_function = loss_function
         self.trainset = trainset
         self.valset = valset
+        self.testset = testset
         self.batch_to_model_function = batch_to_model_function
         self.evaluation_functions = evaluation_functions
         self.target_key_in_batch = target_key_in_batch
@@ -96,6 +99,36 @@ class Trainer():
                 results[name] = fun(predictions, targets)
 
         
+        return results
+    
+    def test(self) -> dict[str, float]:
+        """
+        Test the model
+        Returns
+            dict[str, float]: the test results where the key is the name of the evaluation function and the value is the result of the evaluation function
+        """
+        self.model.eval()
+
+        predictions = []
+        targets = []
+
+        with torch.no_grad():
+            for batch in self.testset:
+                batch = {k: v.to(self.device) for k, v in batch.items()}
+                x = self.batch_to_model_function(batch)
+                target = batch[self.target_key_in_batch]
+                pred = self.model(x)
+
+                predictions.append(pred.detach().cpu())
+                targets.append(target.detach().cpu())
+
+            predictions = torch.cat(predictions, dim=0)
+            targets = torch.cat(targets, dim=0)
+
+            results = {}
+            for name, fun in self.evaluation_functions.items():
+                results[name] = fun(predictions, targets)
+
         return results
                 
 
@@ -169,14 +202,23 @@ class Trainer():
                 print("Early stopping")
                 break
 
+            start_time_epoch = time.time()
+
+        
+        if self.testset is not None:
+            test_results = self.test()
+            print(f"Test: {test_results}")
+            return all_results_training, all_results_validation, all_results_time, test_results
+
         return all_results_training, all_results_validation, all_results_time
     
 
-def visualize_results(results: Tuple[list[dict[str, float]], list[dict[str, float]]],):
+def visualize_results(results: Tuple[list[dict[str, float]], list[dict[str, float]]], loglog = True):
     """
     visualize results as returned by train()
     Args: 
         results: Tuple[list[dict[str, float]], list[dict[str, float]]]: the results
+        loglog: bool: whether to use loglog scale
     """
 
     n_subplots = len(results[0][0])
@@ -187,7 +229,16 @@ def visualize_results(results: Tuple[list[dict[str, float]], list[dict[str, floa
         training = [result[name] for result in results[0]]
         validation = [result[name] for result in results[1]]
 
-        axs[i].plot(training, label="Training")
-        axs[i].plot(validation, label="Validation")
-        axs[i].set_title(name)
-        axs[i].legend()
+        if loglog:
+            axs[i].plot(training, label="Training")
+            axs[i].plot(validation, label="Validation")
+            axs[i].set_title(name)
+            axs[i].set_yscale("log")
+            axs[i].set_xscale("log")
+            axs[i].legend()
+
+        else:
+            axs[i].plot(training, label="Training")
+            axs[i].plot(validation, label="Validation")
+            axs[i].set_title(name)
+            axs[i].legend()
