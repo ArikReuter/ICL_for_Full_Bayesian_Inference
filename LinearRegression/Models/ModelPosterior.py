e89c9f8b38dacaa45215e2b7f2cf28d93aaa0dad
ArikReuter <arik_reuter@gmx.de>
2024-05-26 11:11:43 +0200

    refactor: Update ModelPosteriorFullGaussian2 class to use different arguments

--- LinearRegression/Models/ModelPosterior.py
+++ LinearRegression/Models/ModelPosterior.py
@@ -101,6 +101,8 @@ class ModelPosterior(ABC):
 
 
 
+
+
 class ModelPosteriorFullGaussian(ModelPosterior):
     """
     A class that takes in a model and returns the posterior the model provides
@@ -187,7 +189,62 @@ class ModelPosteriorFullGaussian(ModelPosterior):
         dist = self.pred2posterior(pred)
         samples = dist.sample((n_samples,))
         return samples
-    
+
+
+class ModelPosteriorFullGaussian2(ModelPosteriorFullGaussian):
+    """
+    A class that takes in a model and returns the posterior the model provides
+    This class uses different arguments
+    """ 
+
+    def __init__(self,
+                 cov_reg_factor: float = 0.0,
+                 loss_on_error: float = 1e10,
+                 use_lowrank_normal: bool = False,
+                 diag_transform: callable = torch.exp):
+        """
+        Args:
+            cov_reg_factor: float: the regularization factor for the covariance matrix
+            loss_on_error: float: the loss value to return if an error occurs
+            use_lowrank_normal: bool: whether to use the lowrank normal distribution
+            diag_transform: callable: the transformation to apply to the diagonal of the covariance matrix
+        """
+        self.cov_reg_factor = cov_reg_factor
+        self.loss_on_error = loss_on_error
+        self.use_lowrank_normal = use_lowrank_normal
+        self.diag_transform = diag_transform
+
+        super().__init__(cov_reg_factor, loss_on_error)
+
+
+    def pred2posterior(self, pred) -> torch.distributions.distribution.Distribution:
+        """
+        A method that takes in the models prediction and returns the posterior distribution
+        Args:
+            pred: the models predicctions 
+        Returns:
+            torch.distributions.distribution.Distribution: the posterior distribution
+        """
+
+        mu, cov_factor, cov_diag = pred
+        cov_factor = cov_factor.reshape(mu.shape[0], mu.shape[1], -1)
+        cov_diag = self.diag_transform(cov_diag) + self.cov_reg_factor
+
+        if self.use_lowrank_normal:
+            dist = torch.distributions.LowRankMultivariateNormal(
+                loc = mu,
+                cov_factor = cov_factor,
+                cov_diag = cov_diag
+                )
+        else:
+            cov = cov_factor @ cov_factor.transpose(-1, -2) + torch.diag_embed(cov_diag)
+            
+            dist = torch.distributions.MultivariateNormal(
+                loc = mu,
+                covariance_matrix = cov
+                )
+
+        return dist
 
 
 class ModelPosteriorFullGaussianExpDiag(ModelPosteriorFullGaussian):
@@ -195,6 +252,13 @@ class ModelPosteriorFullGaussianExpDiag(ModelPosteriorFullGaussian):
     A class that takes in a model and returns the posterior the model provides
     It uses the exponential of the diagonal of the covariance matrix
     """
+    def __init__(self, cov_reg_factor: float = 0.0, loss_on_error: float = 1e10):
+        """
+        Args:
+            cov_reg_factor: float: the regularization factor for the covariance matrix
+        """
+        super().__init__(cov_reg_factor, loss_on_error)
+        
 
     def pred2posterior(self, pred) -> torch.distributions.distribution.Distribution:
         """
