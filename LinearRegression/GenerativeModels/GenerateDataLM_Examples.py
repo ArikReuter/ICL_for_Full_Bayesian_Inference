2e1e20afa80f2601c57dc1baea0e724d4ea4b29d
ArikReuter <arik_reuter@gmx.de>
2024-05-27 09:18:18 +0200

    refactor: Update GenerateDataLM_Examples.py to add new linear model probabilistic programs

--- LinearRegression/GenerativeModels/GenerateDataLM_Examples.py
+++ LinearRegression/GenerativeModels/GenerateDataLM_Examples.py
@@ -256,6 +256,8 @@ def make_lm_program_gamma_gamma_reduced_dimensionality(
             sigma_squared_dist = pyro.distributions.Gamma(a1, b1)
             sigma_squared = pyro.sample("sigma_squared", sigma_squared_dist)
 
+            
+
             beta_cov = torch.eye(x.shape[1]) * beta_var # the covariance matrix of the parameters of the linear model
             beta = pyro.sample("beta", pyro.distributions.MultivariateNormal(torch.zeros(x.shape[1]), beta_cov)) # the parameters of the linear model
 
@@ -319,6 +321,106 @@ def make_lm_program_gamma_gamma_augmented(
                         "y": y,
                         "sigma_squared": sigma_squared,
                         "tau": tau,
+                        "beta": beta
+                }
+
+        return multivariate_lm_return_dict
+
+
+def make_lm_program_ig_batched(
+        tau: float = 1.0,
+        a: float = 5.0,
+        b: float = 2.0
+    ) -> 'LM_abstract.pprogram_linear_model_return_dict':
+        """
+        Make a linear model probabilistic program with a gamma prior on sigma_squared and a gamma prior on beta_var.
+        Args: 
+                tau: float: the standard deviation of the prior on beta
+                a: float: the shape parameter of the gamma prior on sigma_squared
+                b: float: the rate parameter of the gamma prior on sigma_squared
+        Returns:
+                LM_abstract.pprogram_linear_model_return_dict: a linear model probabilistic program
+        """
+        def multivariate_lm_return_dict(x: torch.Tensor, y: torch.Tensor = None) -> dict:
+                if x.dim() == 2:
+                        x = x.unsqueeze(0)  # Ensure x is 3D (batch_size, N, P)
+                batch_size, N, P = x.shape
+
+                # Define distributions for the global parameters
+                sigma_squared_dist = dist.InverseGamma(a, b)
+
+
+                beta_cov = torch.eye(P) * (tau ** 2)  # the covariance matrix of the parameters of the linear model
+
+                with pyro.plate("batch", batch_size, dim=-1):   
+                        
+                        sigma_squared = pyro.sample("sigma_squared", sigma_squared_dist).squeeze() # Shape: (batch_size,)
+
+                        #print(beta_cov.shape)
+                        beta_dist = dist.MultivariateNormal(torch.zeros(P), beta_cov)
+                        #print(beta_dist.batch_shape, beta_dist.event_shape)
+                        beta = pyro.sample("beta", beta_dist)  # Shape: (batch_size, P)
+
+                
+                        # Compute mean using matrix multiplication
+                        mean = torch.matmul(x, beta.unsqueeze(-1)).squeeze(-1)  # Shape: (batch_size, N)
+
+
+                        with pyro.plate("data", N):
+                                noise = pyro.sample("noise", dist.Normal(0, sigma_squared))  # Shape: (batch_size, N)
+                        
+                        noise = noise.permute(1, 0)  # Shape: (N, batch_size)
+                        y = mean + noise  # Shape: (batch_size, N)
+
+
+
+                return {
+                                "x": x,
+                                "y": y,
+                                "sigma_squared": sigma_squared,
+                                "beta": beta
+                        }
+
+        return multivariate_lm_return_dict
+
+def make_lm_program_ig(
+        tau: float = 1.0,
+        a: float = 5.0,
+        b: float = 2.0
+    ) -> 'LM_abstract.pprogram_linear_model_return_dict':
+        """
+        Make a linear model probabilistic program with a gamma prior on sigma_squared and a gamma prior on beta_var.
+        This version is not batched!
+
+        Args:
+                tau: float: the standard deviation of the prior on beta
+                a: float: the shape parameter of the gamma prior on sigma_squared
+                b: float: the rate parameter of the gamma prior on sigma_squared
+        Returns:
+                LM_abstract.pprogram_linear_model_return_dict: a linear model probabilistic program
+        """
+        def multivariate_lm_return_dict(x: torch.Tensor, y: torch.Tensor = None) -> dict:
+                # Define distributions for the global parameters
+                sigma_squared_dist = dist.InverseGamma(a, b)
+
+                beta_cov = torch.eye(x.shape[1]) * (tau ** 2)  # the covariance matrix of the parameters of the linear model
+
+                sigma_squared = pyro.sample("sigma_squared", sigma_squared_dist).squeeze()
+
+                beta_dist = dist.MultivariateNormal(torch.zeros(x.shape[1]), beta_cov)
+                beta = pyro.sample("beta", beta_dist)
+
+                # Compute mean using matrix multiplication
+                mean = torch.matmul(x, beta)
+
+                with pyro.plate("data", len(x)):
+                        y = pyro.sample("obs", pyro.distributions.Normal(mean, sigma_squared), obs=y)
+
+
+                return {
+                        "x": x,
+                        "y": y,
+                        "sigma_squared": sigma_squared,
                         "beta": beta
                 }
 
