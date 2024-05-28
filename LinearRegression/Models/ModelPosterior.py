734da90b915e3ed6c3defc93dd1c7c49e61368aa
ArikReuter <arik_reuter@gmx.de>
2024-05-28 09:50:46 +0200

    feat: Add ModelPosteriorMultivariateStundentt class for multivariate student t distribution

--- LinearRegression/Models/ModelPosterior.py
+++ LinearRegression/Models/ModelPosterior.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import torch
 from torch.distributions import LowRankMultivariateNormal
+from pyro.distributions import MultivariateStudentT
 from numpy.linalg import LinAlgError
 
 from PFNExperiments.Training.EvalMetrics import mean, median, std
@@ -278,3 +279,97 @@ class ModelPosteriorFullGaussianExpDiag(ModelPosteriorFullGaussian):
             cov_diag = cov_diag
             )
         return dist
+
+
+class ModelPosteriorMultivariateStundentt(ModelPosterior):
+    """
+    A class that takes in a model and returns the posterior the model provides
+    It uses the multivariate student t distribution
+    """
+
+    def __init__(self, cov_reg_factor:float = 0.0, loss_on_error: float = 1e10):
+        """
+        Args:
+            cov_reg_factor: float: the regularization factor for the covariance matrix
+            loss_on_error: float: the loss value to return if an error occurs
+        """
+        self.cov_reg_factor = cov_reg_factor
+        self.loss_on_error = loss_on_error
+
+    def pred2posterior(self, pred) -> torch.distributions.distribution.Distribution:
+        """
+        A method that takes in the models prediction and returns the posterior distribution
+        Args:
+            pred: the models predicctions 
+        Returns:
+            torch.distributions.distribution.Distribution: the posterior distribution
+        """
+        mu, cov_factor, cov_diag, df = pred
+        cov_factor = cov_factor.reshape(mu.shape[0], mu.shape[1], -1)
+        cov_diag = cov_diag **2 + self.cov_reg_factor
+
+        cov_mat = cov_factor @ cov_factor.transpose(-1, -2) + torch.diag_embed(cov_diag)
+
+        cov_tril = torch.cholesky(cov_mat)
+
+        dist = MultivariateStudentT(loc = mu, scale_tril = cov_tril, df = df)
+
+        return dist
+    
+
+    def negative_log_likelihood(self, pred, target: torch.Tensor) -> torch.Tensor:
+        """
+        A method that takes in the models prediction and the target and returns the negative log likelihood
+        Args:
+            pred: torch.Tensor: the models prediction
+            target: torch.Tensor: the target
+        Returns:
+            torch.Tensor: the negative log likelihood
+        """
+
+        if type(target) == list:
+            assert len(target) == 1, "The target is a list but the length is not 1"
+            target = target[0]
+        
+        try: 
+            dist = self.pred2posterior(pred)
+
+            nll = - dist.log_prob(target)
+
+        except RuntimeError as e:
+            if 'linalg.cholesky' in str(e) or "ValueError" in str(e):
+                print("Caught a _LinAlgError related to Cholesky decomposition: The input is not positive-definite.")
+                print("Error message: ", e)
+            else:
+                raise
+
+            mu, cov_factor, cov_diag, df = pred
+
+            mu_fake = mu*0
+            cov_fake = cov_factor*0
+            cov_diag = cov_diag*0
+            df_fake = 1
+
+            pred_fake = (mu_fake, cov_fake, cov_diag, df_fake)
+            dist = self.pred2posterior(pred_fake)
+
+            nll = - dist.log_prob(target)
+
+
+        return nll.mean()
+    
+
+    def pred2posterior_samples(self, pred, n_samples: int) -> torch.Tensor:
+        """
+        A method that takes in the models prediction and returns samples from the posterior distribution
+        Args:
+            pred: torch.Tensor: the models prediction
+            n_samples: int: the number of samples
+        Returns:
+            torch.Tensor: the samples from the posterior distribution with shape (n_samples, ...) where ... denotes the shape of the samples
+        """
+        dist = self.pred2posterior(pred)
+        samples = dist.sample((n_samples,))
+        return samples
+        
+
