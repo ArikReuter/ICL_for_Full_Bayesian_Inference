11015877fa44dae970982a588b478f30f34dec34
ArikReuter <arik_reuter@gmx.de>
2024-06-17 15:58:22 +0200

    fixed issues with sinkhorn solver

--- Training/FlowMatching/Couplings.py
+++ Training/FlowMatching/Couplings.py
@@ -8,11 +8,12 @@ class MiniBatchOTCoupling:
 
     def __init__(
             self,
-            solver:str = "sinkhorn",
+            solver:str = "emd",
             solver_params: dict = {
-                "reg": 1e-10,
-                "numItermax": 1000
-                }
+                #"reg": 1e-10,
+                #"numItermax": 1000
+                },
+            sample_with_replacement: bool = False
     ):
         """
         Args:
@@ -23,23 +24,9 @@ class MiniBatchOTCoupling:
 
         self.solver = solver
         self.solver_params = solver_params
+        self.replace = sample_with_replacement
 
-    
-    def compute_similarity_mat(self, z_0: torch.tensor, z_t: torch.tensor) -> torch.tensor:
-        """
-        Compute the similarity matrix between z_0 and z_t using the squared L2 norm
-        Args:
-            z_0: torch.tensor: the tensor from the base distribution of shape (batch_size, n_features)
-            z_t: torch.tensor: the tensor from the data distribution of shape (batch_size, n_features)
-
-        Returns:
-        """
-        assert z_0.shape == z_t.shape, f"z_0 shape {z_0.shape} does not match z_t shape {z_t.shape}"
-
-        sim_mat = torch.cdist(z_0, z_t, p=2)**2 # (batch_size, batch_size)
-
-        return sim_mat
-    
+        
     def compute_coupling(self, z_0: torch.tensor, z_t: torch.tensor) -> torch.tensor:
         """
         Compute the coupling between z_0 and z_t
@@ -50,16 +37,14 @@ class MiniBatchOTCoupling:
         Returns:
             coupling: torch.tensor: the coupling between z_0 and z_t of shape (batch_size, batch_size)
         """
-        sim_mat = self.compute_similarity_mat(z_0, z_t)
-
-        cost_mat = torch.max(sim_mat) - sim_mat
+        dist_mat = ot.dist(z_0, z_t, metric="euclidean") # (batch_size, batch_size)
 
         a = torch.ones(z_0.shape[0])/z_0.shape[0] #uniform weights on samples from z_0 and z_t
         a = a.to(z_0.device)
         if self.solver == "sinkhorn":
-            coupling = ot.sinkhorn(a, a, cost_mat, **self.solver_params)
+            coupling = ot.bregman.sinkhorn(a, a, dist_mat, **self.solver_params)
         elif self.solver == "emd":
-            coupling = ot.emd(a, a, cost_mat, **self.solver_params)
+            coupling = ot.emd(a, a, dist_mat, **self.solver_params)
         
         return coupling
     
@@ -79,7 +64,7 @@ class MiniBatchOTCoupling:
 
         return assignment
     
-    def get_assignment_from_coupling_sampling(self, coupling: torch.tensor, replace: bool = False) -> torch.tensor:
+    def get_assignment_from_coupling_sampling(self, coupling: torch.tensor) -> torch.tensor:
         """
         Get the assignment from the coupling
         Args:
@@ -88,7 +73,7 @@ class MiniBatchOTCoupling:
         Returns:
             assignment: torch.tensor: the assignment from the coupling of shape (batch_size,)
         """
-        assignment = torch.multinomial(coupling, 1, replacement=replace).squeeze()
+        assignment = torch.multinomial(coupling, 1, replacement=self.replace).squeeze()
 
         return assignment
     
