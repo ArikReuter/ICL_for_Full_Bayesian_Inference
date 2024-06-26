45522e4f3cbf8835743d3e0701db5d291a92e652
ArikReuter <arik_reuter@gmx.de>
2024-06-26 09:52:22 +0200

    added trainer that simultaneously uses NLL and Flow matching

---
+++ Training/FlowMatching/CFMLossOTGaussianBase_plus_NLL.py
@@ -0,0 +1,62 @@
+import torch 
+
+from PFNExperiments.Training.FlowMatching.CFMLossOTGaussianBase import CFMLossOTGaussianBase
+from PFNExperiments.LinearRegression.Models.ModelPosterior import ModelPosterior
+
+class CFMLossOTGaussianBase(CFMLossOTGaussianBase):
+    """
+    Implementation of the conditional flow matching loss with the optimal transport probability paths and an arbitrary Gaussian base distribution
+    """
+
+   
+    def __init__(
+            self,
+            weight_nll: float,
+            NLL_Loss_function: callable,
+            sigma_min: float = 1e-4,
+    ):
+        """
+        Args:  
+            weight_nll: float: the weight of the negative log likelihood, has to be between 0 and 1
+            nll_loss_function: callable: the negative log likelihood loss function
+            sigma_min: float: the minimum value of the sigma
+        """
+        super(CFMLossOTGaussianBase, self).__init__()
+
+        assert 0 <= weight_nll <= 1, f"weight_nll has to be between 0 and 1, but is {weight_nll}"
+
+        self.weight_nll = weight_nll
+        self.NLL_Loss_function = NLL_Loss_function
+        self.sigma_min = sigma_min
+
+
+    def __call__(self,
+                 pred_for_nll_loss : torch.Tensor,
+                 vector_field_prediction: torch.Tensor,
+                 z_0_a: torch.Tensor,
+                 z_0_b: torch.Tensor,
+                 z_1: torch.Tensor,
+                 t: float,
+
+    ) -> torch.Tensor:
+        """
+        Implement the computation of the conditional flow matching loss
+        Args:
+            pred_for_nll_loss: torch.Tensor: the prediction of the negative log likelihood computed by the model
+            vector_field_prediction: torch.Tensor: the prediction of the vector field based on \psi_t(\vz_0|\vz_1) computed by the model
+            z_0_a: torch.Tensor: the input tensor sampled from the base distribution
+            z_0_b: torch.Tensor: another input tensor sampled from a standard normal distribution
+            z_1: torch.Tensor: the tensor from the data distribution
+            t: float: the time step
+        """
+
+        target = self.sigma_min * z_0_b + z_1 - z_0_a
+
+        loss_flow_matching = torch.mean((vector_field_prediction - target)**2)
+
+        loss_nll = self.NLL_Loss_function(pred_for_nll_loss, z_1)
+
+        loss = (1-self.weight_nll) * loss_flow_matching + self.weight_nll * loss_nll
+
+        return loss
+
