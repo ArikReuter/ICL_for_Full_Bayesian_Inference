b3029a61850e0202fb1826a50bfc6680fc059c12
ArikReuter <arik_reuter@gmx.de>
2024-07-28 15:57:20 +0200

    small fix

--- LinearRegression/GenerativeModels/GenerateX.py
+++ LinearRegression/GenerativeModels/GenerateX.py
@@ -123,3 +123,87 @@ def simulate_X_uniform_discretized2(
             res[i, :, j] = res[i, randperm, j]
 
     return res
+
+
+def simulate_X_uniform_discretized3(n: int, p: int, batch_size: int = 1):
+    """
+    Simulate covariates from a uniform distribution on the unit interval that are discretized.
+    
+    Args:
+        n: int: the number of observations
+        p: int: the number of covariates
+        batch_size: int: the batch size
+    """
+    
+    if n == 100:
+        n1 = int(n * 0.2)
+        n2 = n - n1 - 1
+        prob_vec = [0] * n1 + [1] * n2 + [10]
+        prob_vec = torch.tensor(prob_vec)
+    else:
+        n1 = int(n * 0.5)
+        n2 = n - n1 - 1
+        prob_vec = [0] * n1 + [1] * n2 + [10]
+        prob_vec = torch.tensor(prob_vec)
+    
+    n_diff_values_dist = torch.distributions.Categorical(prob_vec)
+    
+    res = torch.rand(batch_size, n, p)
+    
+    n_diff_values_samples = n_diff_values_dist.sample((batch_size, p)) + 1
+    randperm = torch.randperm(n).unsqueeze(0).unsqueeze(2) % n_diff_values_samples.unsqueeze(1)
+    
+    res = res.scatter(1, randperm.expand(batch_size, n, p), res.clone())
+    
+    return res
+
+
+def simulate_X_uniform_discretized4(n: int, p: int, batch_size: int = 1):
+    """
+    Simulate covariates from a uniform distribution on the unit interval that are discretized.
+    
+    Args:
+        n: int: the number of observations
+        p: int: the number of covariates
+        batch_size: int: the batch size
+    """
+    
+    if n == 100:
+        n1 = int(n * 0.2)
+        n2 = n - n1 - 1
+        prob_vec = [0] * n1 + [1] * n2 + [10]
+        prob_vec = torch.tensor(prob_vec)
+    else:
+        n1 = int(n * 0.5)
+        n2 = n - n1 - 1
+        prob_vec = [0] * n1 + [1] * n2 + [10]
+        prob_vec = torch.tensor(prob_vec)
+    
+    n_diff_values_dist = torch.distributions.Categorical(prob_vec)
+    
+    datasets = torch.rand(batch_size, n, p)
+    
+
+    bs, N, P = datasets.shape
+    
+    # Sample the number of bins for each dataset and each feature
+    n_bins = 10* n_diff_values_dist.sample((bs, P)).int() + 2  # Ensure at least 2 bins for meaningful discretization
+
+    # Create an array to hold the discretized datasets
+    discretized_datasets = torch.zeros_like(datasets)
+
+    for i in range(bs):
+        for j in range(P):
+            # Sample border points and sort them to create bins
+            bins = torch.sort(torch.rand(n_bins[i, j] - 1))[0]
+            bins = torch.cat((torch.tensor([0.0]), bins, torch.tensor([1.0])))
+            
+            # Digitize the data points according to the bins
+            digitized = torch.bucketize(datasets[i, :, j], bins, right=True) - 1
+
+            # Map the digitized data points back to the bin midpoints
+            bin_mids = (bins[:-1] + bins[1:]) / 2.0
+            discretized_datasets[i, :, j] = bin_mids[digitized]
+    
+    return discretized_datasets
+    
