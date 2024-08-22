bdccc78efca7ce9d31322000dfbb0f70a96701f1
ArikReuter <arik_reuter@gmx.de>
2024-08-22 18:15:08 +0200

    added automatic config generation

---
+++ Experiments/Configs/ConfigsCFMLinearRegression/BasicConfigCreatorColab.py
@@ -0,0 +1,129 @@
+import configparser
+import time 
+
+from ModelConfigCreator import make_basic_model_config
+
+class BasicConfigCreatorColab():
+    """
+    A class to create a basic configuration file for experiments in Colab.
+    """
+    
+    def __init__(
+            self,
+            config_name: str = None):
+        """
+        Constructor of the BasicConfigCreator class.
+        """
+        self.config = configparser.ConfigParser()
+        self.config["confic_created_on"] = time.asctime()
+
+        if config_name is None:
+            self.config_name = "basic_config"
+        else:
+            self.config_name = config_name
+
+        self.config["name"] = self.config_name
+
+        
+    def set_basic_params(self):
+        """
+        Set the basic parameters for the configuration file.
+        """
+
+        self.config['BASIC'] = {
+            "N": 50,   # number of samples per in-context dataset
+            "P": 5,    # number of features per in-context dataset
+            "Batch_size": 1024,  # batch size for training
+            "N_epochs": 100,  # number of epochs for training
+            "N_samples_per_epoch": 500_000, # number of samples to use per epoch
+            "N_samples_to_generate_at_once": 250_000, # number of samples to generate at once
+            "Shuffle": False, # shuffle the data before training
+            "Save_path": "/content/drive/MyDrive/PFN_Experiments/Training_RunsCFM", # path to save the model
+            "Train_frac": 0.5, # fraction of the data to use for training
+            "Val_frac": 0.1 # fraction of the data to use for validation
+        }
+
+
+    def set_data_generation_params(self):
+        """
+        Set the data generation parameters for the configuration file.
+        """
+
+        self.config['DATA_GENERATION'] = {
+            "Pprogram": None, # probabilistic program to generate the data
+            "Pprogram_batched": None, # probabilistic program to generate the data in batches
+            "Use_intercept": False, # whether to use an intercept in the model
+            "Scheduler_behaviour": "All_constant", # behaviour of the scheduler of the probabilistic program's parameters
+            "Generate_X_behaviour": "TabPFNX_extended1" # behaviour of the data generation process
+        }
+
+        self.config['PPROGRAM_PARAMS'] = None # parameters for the probabilistic program
+
+
+    def set_model_params(
+            self
+        ):
+        """
+        Set the model parameters for the configuration file.
+
+        """
+        P = self.config['BASIC']['P']
+
+
+        self.config['MODEL'] = make_basic_model_config(
+            P = P,
+            use_intercept = self.config['DATA_GENERATION']['Use_intercept']
+        )
+
+    def set_training_params(
+            self
+    ):
+        """
+        Set the training parameters for the configuration file.
+        """
+
+        self.config['TRAINING'] = {
+            "Learning_rate": 1e-6, # learning rate for training
+            "Weight_decay": 1e-5, # weight decay for training
+            "Scheduler": "OneCycleLR", # scheduler for the learning rate
+            "Scheduler_params": {
+                "max_lr": 5e-4, # maximum learning rate
+                "epochs": self.config['BASIC']['N_epochs'], # number of epochs
+                "steps_per_epoch": self.config['BASIC']['N_samples_per_epoch'] // self.config['BASIC']['Batch_size'], # steps per epoch
+                "pct_start": 0.1, # percentage of the cycle spent increasing the learning rate
+                "div_factor": 25.0, # factor by which the maximum learning rate is divided
+                "final_div_factor": 1e4 # factor by which the maximum learning rate is divided at the end
+            },
+            "early_stopping_patience": 100_000, # patience for early stopping -> set to a large number to disable early stopping
+            "max_grad_norm": 1.0, # maximum gradient norm
+            "trainer_save_path": None,
+        }
+
+    def set_evaluation_params(self):
+        """
+        Set the evaluation parameters for the configuration file.
+        """
+
+        self.config["EVALUATION"] = {
+            "N_samples_per_model": 1_000, # number of samples to generate per model
+            "N_synthetic_cases": 50, # number of synthetic cases to generate and evaluate on
+            "Real_world_eval": "Basic1", # real world evaluation to perform,
+            "n_evaluation_cases_real_world": "All" # number of real world evaluation cases to use
+        }
+
+    def set_full_model_params(self):
+        """
+        Set the params for the full model.
+        """
+
+        P = self.config['BASIC']['P']   
+        self.config['FULL_MODEL'] = {
+            "sample_name": "beta",
+            "sample_shape": (P+1,) if self.config['DATA_GENERATION']['Use_intercept'] else (P,),
+            "n_samples": 1_000, # number of samples to generate to compare for each case
+            "batch_size": self.config['BASIC']['Batch_size'], # batch size for generating samples
+            "solve_adjoint": True, # whether to solve the adjoint ODE
+            "atol": 1e-7,
+            "rtol": 1e-7
+        }
+
---
+++ Experiments/Configs/ConfigsCFMLinearRegression/ModelConfigCreator.py
@@ -0,0 +1,39 @@
+
+def make_basic_model_config(P:int, use_intercept:bool = False) -> dict:
+    """
+    make a basic model configuration for the CFM model.
+    Args:
+    P: int: number of features per in-context dataset
+    use_intercept: bool: whether to use an intercept in the model
+    Returns:
+    dict: the model configuration
+    """
+    P1 = P + 1 if use_intercept else P
+    config = {
+            "Type": "TransformerCNFConditionalDecoder", 
+            "n_input_features_encoder": P+1,
+            "n_input_features_decoder": P1,
+            "d_model_encoder": 512,
+            "d_model_decoder": 512,
+            "n_heads_encoder": 8,
+            "n_heads_decoder": 8,
+            "d_ff_encoder": 1024,
+            "d_ff_decoder": 1024,
+            "dropout_encoder": 0.1,
+            "dropout_decoder": 0.1,
+            "n_conditional_input_features":  1,
+            "n_condition_features": 512,
+            "n_layers_condition_embedding": 3,
+            "n_layers_encoder": 8,
+            "n_layers_decoder": 6,
+            "use_positional_encoding_encoder": True,
+            "use_positional_encoding_decoder": False,
+            "use_self_attention_decoder": False,
+            "output_dim": P1,
+            "d_final_processing": 512,
+            "n_final_layers": 3,
+            "dropout_final": 0.1,
+            "treat_z_as_sequence": False,
+        }
+    
+    return config
