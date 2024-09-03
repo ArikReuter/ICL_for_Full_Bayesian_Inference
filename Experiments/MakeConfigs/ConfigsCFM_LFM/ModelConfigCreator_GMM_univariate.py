548077d64879c8c6a473a6a7b80f93c75a36292c
ArikReuter <arik_reuter@gmx.de>
2024-09-03 11:09:53 +0200

    small updates

--- Experiments/MakeConfigs/ConfigsCFM_LFM/ModelConfigCreator_GMM_univariate.py
+++ Experiments/MakeConfigs/ConfigsCFM_LFM/ModelConfigCreator_GMM_univariate.py
@@ -1,17 +1,17 @@
-    make_basic_model_config(P:int, use_intercept:bool = False) -> dict:-
+
+def make_basic_model_config(P:int) -> dict:
     """
     make a basic model configuration for the CFM model.
     Args:
     P: int: number of features per in-context dataset
     use_intercept: bool: whether to use an intercept in the model
-    Returns:
+    Returns:-
     dict: the model configuration
-    """
-    P1 = P + 1 if use_intercept else P
+    """ 
     config = {
             "Type": "TransformerCNFConditionalDecoder", 
-            "n_input_features_encoder": P+1,
-            "n_input_features_decoder": P1,
+            "n_input_features_encoder": 1,
+            "n_input_features_decoder": 2*P,
             "d_model_encoder": 512,
             "d_model_decoder": 512,
             "n_heads_encoder": 8,
@@ -19,7 +19,7 @@
             "d_ff_encoder": 1024,
             "d_ff_decoder": 1024,
             "dropout_encoder": 0.1,
-            "dropout_deco": 0.1,-
+            "dropout_decoder": 0.1,
             "n_conditional_input_features":  1,
             "n_condition_features": 512,
             "n_layers_condition_embedding": 3,
@@ -28,7 +28,7 @@
             "use_positional_encoding_encoder": True,
             "use_positional_encoding_decoder": False,
             "use_self_attention_decoder": False,
-            "output_dim": P1,
+            "output_dim": 2*P,
             "d_final_processing": 512,
             "n_final_layers": 3,
             "dropout_final": 0.1,
