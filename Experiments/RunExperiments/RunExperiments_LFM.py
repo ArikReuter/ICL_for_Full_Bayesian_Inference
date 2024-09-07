a3fea259f954aa9678e0f7f62b4dfb545441cb66
ArikReuter <arik_reuter@gmx.de>
2024-09-07 17:05:22 +0200

    sf

--- Experiments/RunExperiments/RunExperiments_LFM.py
+++ Experiments/RunExperiments/RunExperiments_LFM.py
@@ -301,7 +301,7 @@ class RunExperiments_LFM(RunExperiments):
         
         print_code(self.pprogram1)
 
-        if "discrete_z" in ast.literal_eval(self.config["EVALUATION"]["discrete_z"]):
+        if "discrete_z" in ast.literal_eval(self.config["EVALUATION"]).keys():
             discrete_z = string2bool(ast.literal_eval(self.config["EVALUATION"]["discrete_z"]))
             print(f"discrete_z: {discrete_z}")
         else:
