6fba68906bee07b9c4c38cdb154e32e381d6c852
ArikReuter <arik_reuter@gmx.de>
2024-09-15 21:14:02 +0200

    sf

--- Experiments/RunExperiments/RunExperiments_LFM.py
+++ Experiments/RunExperiments/RunExperiments_LFM.py
@@ -119,14 +119,17 @@ class RunExperiments_LFM(RunExperiments):
             pprogram_covariates = self.generate_X,
         )
 
-        self.check_model_res = self.data_generator.check_model(
-            n_samples_per_epoch=N_SAMPLES_PER_EPOCH,
-            epochs_to_check = [0, N_EPOCHS-1],
-            p = P,
-            n = N,
-            used_batch_samples = 3,
-            save_path_plots=self.config["BASIC"]["Save_path"] + "/check_model"
-        )
+        try:
+            self.check_model_res = self.data_generator.check_model(
+                n_samples_per_epoch=N_SAMPLES_PER_EPOCH,
+                epochs_to_check = [0, N_EPOCHS-1],
+                p = P,
+                n = N,
+                used_batch_samples = 3,
+                save_path_plots=self.config["BASIC"]["Save_path"] + "/check_model"
+            )
+        except Exception as e:
+            print(f"Error in check_model: {e}")
         self.epoch_loader = self.data_generator.make_epoch_loader(
             n = N,
             p = P,
