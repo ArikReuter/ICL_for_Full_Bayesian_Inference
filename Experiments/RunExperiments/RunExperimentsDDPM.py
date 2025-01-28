66fe6f55a75d81fc876db108853b61e1dd1b49db
ArikReuter <arik_reuter@gmx.de>
2025-01-28 09:34:20 +0100

    sf

--- Experiments/RunExperiments/RunExperimentsDDPM.py
+++ Experiments/RunExperiments/RunExperimentsDDPM.py
@@ -285,6 +285,13 @@ class RunExperiments():
                 beta_min = float(train_config["beta_min"]),
                 beta_max = float(train_config["beta_max"]),
             )
+
+        elif train_config["Loss_function"] == "DDPMLossDiffusionVP":
+            self.loss_function = DDPMLossDiffusionVP(
+                epsilon_for_t = float(train_config["epsilon_for_t"]),
+                beta_min = float(train_config["beta_min"]),
+                beta_max = float(train_config["beta_max"]),
+            )
         else:
             raise ValueError(f"Loss function {train_config['Loss_function']} not implemented yet!")
         
